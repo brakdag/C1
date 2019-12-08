69fba51bc9cc7f26b8ad6bbb3f2a31a6afa98277
brakdag <brakdag@gmail.com>
2019-12-08 17:13:44 -0300

    a

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send("ok");
 });
 
 app.listen(server_port, server_ip_address, function () {
-  console.log('Example app listening on port 3000!');
+  console.log(process.stdout);
 });
