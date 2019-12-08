2d9c4b33c157d3ce00f0cc55d616fcb61fd0d79f
brakdag <brakdag@gmail.com>
2019-12-08 17:07:59 -0300

    a

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { spawn } = require('child_process');
 const path_linux='./lib/w1'
 const path_windows='./lib/w2.exe'
 
-const ruta = path_windows
+const ruta = path_linux
 
 const child = spawn(ruta, ['-l']);
 child.stdout.pipe(process.stdout);
