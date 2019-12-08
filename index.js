e09f1754b8d1fbefbfec3c17842b69b367839a03
brakdag <brakdag@gmail.com>
2019-12-08 18:00:28 -0300

    cambios

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const { spawn } = require('child_process');
 const path_linux='./lib/w1'
 const path_windows='./lib/w2.exe'
 
-const ruta = path_linux
-
+try{
+    const ruta = path_linux
+}catch(e){console.log(e)}
 const child = spawn(ruta, ['-l']);
 //child.stdout.pipe(process.stdout);
 
